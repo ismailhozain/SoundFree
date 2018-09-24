0f29ba11aa31c5d492ab919d1e8c883c0a029be2
Ismail Hozain <ismailhozain@gmail.com>
2018-09-24 11:22:39 -0500

    ADDED BASICALLY EVERYTHING

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="md">
+            <b-navbar-brand href="/">Quietspace</b-navbar-brand>
+            <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+            <b-collapse is-nav id="nav_collapse">
+                <b-navbar-nav>
+                    <b-nav-item to="/about">About</b-nav-item>
+                    <b-nav-item to="/login">Login</b-nav-item>
+                </b-navbar-nav>
+                <b-navbar-nav>
+                    <b-nav-item to="/add-workplace">Add Workplace</b-nav-item>
+                    <b-nav-item to="/join-event">Find Workplace</b-nav-item>
+               </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+        <router-view class="p-2"/>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+  		<h1 class="text-center">Quietspace finds the best businesses for you to study or work.</h1>
+  		<div>
+  			//stick image slideshow here somehow
+  		</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
