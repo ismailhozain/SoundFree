bc6fa621862f09d5af743691209c975846773c56
Ismail Hozain <ismailhozain@gmail.com>
2018-09-24 12:02:32 -0500

    Added about page info, basic things only

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,26 @@
 <template>
+	<div>
+		<div class="theTeamTitlediv" id="theTeamTitleDivId">
+			<h1 class="text-center">The Team Behind Soundfree</h1>
+		</div>	
+			<div>
+				<div class="aboutIsmailDiv text-center">
+					<h2>Ismail Hozain</h2>
+						<p>Ismail is a freshman at Mckinney High School and has been programming front end development for a year and a half.</p>
+				</div>	
+				<div class="aboutJacobDiv text-center">
+					<h2>Jacob Hicks</h2>
+						<p>Jacob is a sophomore at Mckinney High School.____________</p>
+				</div>
+				<div class="aboutSaffatDiv text-center">	
+					<h2>Saffat Ahmed</h2>
+						<p>Saffat is a senior at Mckinney high school.____________</p>
+				</div>
+				<div class="aboutEvanDiv text-center">
+					<h2>Evan Lastname</h2>
+						<p>Evan is a senior at Mckinney High School._____________</p>
+				</div>
+			</div>	
+	</div>			
 
 </template>
